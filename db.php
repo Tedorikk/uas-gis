e539f958c9e90b80ee7d016e3fb2809c11df6604
Jaka Sampoerna <h1101231033@student.untan.ac.id>
2025-06-05 14:44:36 +0700

    commit dulu

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "db_uas_sigd"; // Ganti sesuai database Anda
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
